23f4df3e8ef383460647f8909fe738cc2fe2253a
DenitKO <motovdenis@mail.ru>
2022-09-18 19:13:27 +0300

    added not worked programm

--- 1seminarFinalWork/Program.cs
+++ 1seminarFinalWork/Program.cs
@@ -16,3 +16,39 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string FillArray (string[] text, int j)
+{
+    for (int i = 0; i < j; i++)
+    {
+        text[j] = Console.ReadLine();
+    }
+}
+
+string[] CutArrayString (string[] text, out string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3)
+            text[i] = array[j];
+    }
+}
+
+void PrintStringArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+int count = 0;
+string[] mytext;
+Console.WriteLine("Введите количество вводимых строк");
+int count = Convert.ToInt32(Console.ReadLine());
+FillArray(mytext, count);
+string[] outext;
+CutArrayString(mytext, outext);
+PrintStringArray(outext);
+
+
